ce198b994b1addaa637da65fb9fe31dbc00ac5d1
“Ganesh” <ganeshprince@gmail.com>
2022-12-08 06:46:49 +0000

    commit 3

--- copy.py
+++ copy.py
@@ -2,7 +2,7 @@ num1 = input('Enter first number: ')
 num2 = input('Enter second number: ')
 
 # Add two numbers
-sum = float(numb1) + float(numb2)
+sum = float(numb1) + float(numbe2)
 
 # Display the sum
-print('The sum of {0} and {1} is {2}'.format(numb1, numb2, sum))
+print('The sum of {0} and {1} is {2}'.format(numb1, numbe2, sum))
